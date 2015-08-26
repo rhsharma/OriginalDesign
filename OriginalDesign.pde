89dcbf7b8451dd47af66e597ed7f7552b918a9e8
unknown <rhsharma@697-334-eowyn.sf.k12.internal>
2015-08-26 11:32:31 -0700

    Added title, heading, and footer along with the Deathly Hallows picture.

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,15 @@
 void setup()
 {
+	background(255, 255, 255);
   size(100,100);
+  noFill();
 }
 void draw()
 {
+	stroke(0, 0, 0);
+	triangle(50, 0, 0, 75, 100, 75);
+	line(50, 0, 50, 75);
   ellipse(50,50,50,50);
+
 }
 
---
+++ build-tmp/source/OriginalDesign.java
@@ -0,0 +1,40 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
+{
+	background(255, 255, 255);
+  size(100,100);
+  noFill();
+}
+public void draw()
+{
+	stroke(0, 0, 0);
+	triangle(50, 0, 0, 75, 100, 75);
+	line(50, 0, 50, 75);
+  ellipse(50,50,50,50);
+
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
